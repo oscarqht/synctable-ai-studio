9354ed80a24cb96364a427653e4e0b00f2b451ac
Oscar <oscarqht@gmail.com>
2026-08-24 14:58:58 +0800

    feat(desktop): add dev/build/start scripts and restore browser icon assets

--- apps/synctable-desktop/electrobun.config.ts
+++ apps/synctable-desktop/electrobun.config.ts
@@ -20,6 +20,12 @@ export default {
     copy: {
       "src/mainview/index.html": "views/mainview/index.html",
       "src/mainview/style.css": "views/mainview/style.css",
+      "src/mainview/browser-arc.png": "views/mainview/browser-arc.png",
+      "src/mainview/browser-chrome.png": "views/mainview/browser-chrome.png",
+      "src/mainview/browser-dia.png": "views/mainview/browser-dia.png",
+      "src/mainview/browser-firefox.png": "views/mainview/browser-firefox.png",
+      "src/mainview/browser-vivaldi.png": "views/mainview/browser-vivaldi.png",
+      "src/mainview/browser-zen.png": "views/mainview/browser-zen.png",
       "../../docs/poster.jpeg": "views/mainview/assets/poster.jpeg",
       "../../docs/logo.png": "views/mainview/assets/logo.png",
       "src/native/bin/dia-db-reader": "bin/dia-db-reader",
